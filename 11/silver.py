89989642b458461aee2b47ff95575cc2f4b42da3
Rickard Nyman <zlowbie@gmail.com>
2017-12-11 21:34:00 +0100

    elva

---
+++ 11/silver.py
@@ -0,0 +1,46 @@
+def reduction(ring, k1, k2, kres = None):
+    l = len(ring)
+    k1 %= l
+    k2 %= l
+    amount = min(ring[k1], ring[k2])
+    ring[k1] -= amount
+    ring[k2] -= amount
+    if kres != None:
+        kres %= l
+        ring[kres] += amount
+    return ring
+
+def calc(steps):
+    keys = ['nw', 'n', 'ne', 'se', 's', 'sw']
+    totalSteps = {}
+    for k in keys:
+        totalSteps[k] = 0
+    for s in steps:
+        totalSteps[s] += 1
+    ring = [totalSteps[k] for k in keys]
+    before = ring
+    l = -1
+    while sum(ring) != l:
+        l = sum(ring)
+        for i in range(6):
+            ring = reduction(ring, i, i+3)
+            ring = reduction(ring, i, i+2, i+1)
+    return sum(ring)
+
+def read(filename):
+    with open(filename, 'r') as f:
+        line = f.readline().rstrip().split(',')
+        return line
+
+def main(filename):
+    print calc(read(filename))
+
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) < 2):
+        print('missing input parameter')
+        exit()
+    for f in sys.argv[1:]:
+        main(f)
+
+
---
+++ 11/test_silver.py
@@ -0,0 +1,15 @@
+import unittest
+import silver as target
+
+class Tester(unittest.TestCase):
+    def test_ex_1(self):
+        self.assertEqual(target.calc(['ne','ne','ne']), 3)
+    def test_ex_2(self):
+        self.assertEqual(target.calc(['ne','ne','sw', 'sw']), 0)
+    def test_ex_3(self):
+        self.assertEqual(target.calc(['ne','ne','s', 's']), 2)
+    def test_ex_4(self):
+        self.assertEqual(target.calc(['se','sw','se', 'sw', 'sw']), 3)
+
+if __name__ == '__main__':
+    unittest.main()
